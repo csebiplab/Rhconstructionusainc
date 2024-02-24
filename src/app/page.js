f81fa8e0e870ec8a6597751b5c4cb06739926497
Oliulla <razasm29@gmail.com>
2024-02-24 16:35:11 +0600

    :metadata: Added meta description

--- src/app/page.js
+++ src/app/page.js
@@ -7,20 +7,20 @@ export async function generateMetadata() {
   const headersList = headers();
   const pathname = headersList.get("x-invoke-path") || "";
   try {
-    const {data}= await API.get('/pages',{
-      params:{
-        route:pathname
+    const { data } = await API.get('/pages', {
+      params: {
+        route: pathname
       }
     });
-    if(!data?.length) throw "no route found."
+    if (!data?.length) throw "no route found."
     return {
       title: data?.[0]?.title,
       keywords: data?.[0]?.keywords,
       description: data?.[0]?.description,
     }
   } catch (error) {
-   console.log(error.message);
-   return {
+    console.log(error.message);
+    return {
       title: "Home | RH Construction USA",
       keywords: "Home, RH Construction USA",
       description: "As one of the leading construction companies in Brooklyn, RH Construction USA Inc. offers top-notch general contractor services in Brooklyn."
