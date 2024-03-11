91f6ca87f8911426775fe0234ed831335b2ac9bc
tonmoy <tonmoy3673@gmail.com>
2024-03-11 23:44:38 +0600

    set achievement section

--- src/components/__home_components/Rating.jsx
+++ src/components/__home_components/Rating.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { FaStar, FaStarHalfAlt } from "react-icons/fa";
 import { AiOutlineStar } from "react-icons/ai";
+import vector from "@/components/__home_components/Images/Vector.png";
+import Progress from "@/components/__home_components/Images/Section.png";
+import Achieve from "@/components/__home_components/Images/Achieve.png";
 
 const Rating = () => {
   let stars = 5;
@@ -21,15 +24,13 @@ const Rating = () => {
   });
   return (
     <div>
-      <div className="border-2 md:w-8/12 w-full mx-auto p-6 rounded-md ">
+      <div className="border-2 md:w-10/12 w-full mx-auto p-6 rounded-md ">
         <h3 className="py-4 font-bold text-2xl">Rating </h3>
-        <div className="grid md:grid-cols-3 grid-cols-1 px-8">
+        <div className="grid md:grid-cols-3 grid-cols-1 px-8 justify-center items-center gap-6">
           {/* ============ fixed rating ========== */}
           <div>
-            <div className="w-[60px] h-[60px] border-gray-300 border-2 rounded-full mb-2 ">
-              <h3 className="text-base text-orange-600 mx-auto flex items-center justify-center">
-                4.9
-              </h3>
+            <div className="w-[60px] h-[60px] border-gray-300 border-2 rounded-full mb-2 flex items-center justify-center">
+              <h3 className="text-base text-[#FAA500] mx-auto">4.9</h3>
             </div>
             <div className="flex items-center gap-1">{ratingStar}</div>
             <h3 className="font-semibold text-sm py-1">61 Verified Reviews*</h3>
@@ -37,13 +38,16 @@ const Rating = () => {
 
           {/* =================== Rating Progress bar ================ */}
           <div>
-            <h3>Rating Progress</h3>
+            <img src={Progress.src} />
           </div>
 
           {/* ============= Highly Rated ============ */}
           <div>
             <div>
-              <h4 className="text-[#000] font-bold mb-3">Highly rated for</h4>
+              <div className="flex">
+                <h4 className="text-[#000] font-bold mb-3">Highly rated for</h4>
+                <img src={vector.src} className="w-[22px] h-[24px] ms-3" />
+              </div>
               <div className="flex justify-center">
                 <h5 className="me-4 border-2 bg-[#EAEAEA] font-semibold text-[#000] text-sm px-2">
                   Quality
@@ -62,6 +66,24 @@ const Rating = () => {
           *Reviews imported from Google have not been individually verified by
           HomeAdvisor.
         </p>
+      </div>
+      <div>
+        {/* ================== AchieveMent =============== */}
+        <div className="grid md:grid-cols-2 grid-cols-1 gap-4 md:w-10/12 w-full mx-auto py-8 justify-center items-center">
+          <div className="w-4/12">
+            <div className="border-2 rounded-md p-4 text-center">
+              <h3 className="text-2xl">98%</h3>
+              <p>Recommended</p>
+            </div>
+            <div className="border-2 rounded-md p-4 text-center w-3/12">
+              <h3 className="text-2xl">4</h3>
+              <p>Years In Business</p>
+            </div>
+          </div>
+          <div className="w-8/12">
+            <img src={Achieve.src} />
+          </div>
+        </div>
       </div>
     </div>
   );
