1028edb793cac8a276c555515aed17125562452d
tonmoy <tonmoy3673@gmail.com>
2024-03-14 00:43:47 +0600

    built hero banner

--- src/components/__home_components/Hero.jsx
+++ src/components/__home_components/Hero.jsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 
 const Hero = () => {
   return (
@@ -6,19 +5,36 @@ const Hero = () => {
       <div
         className="hero-section"
       >
-        <div className="flex items-center">
+        <div className="md:flex items-center relative">
           <div className="w-full md:w-7/12">
-
+            <div className="">
+            <h1 className="leading-10 text-[2rem]">
+                About Our General Contractor Company
+                <br />
+                with
+                <span className="text-white ml-2">
+                  R H Construction USA Inc
+                </span>
+              </h1>
+              <p className="text-base mb-5 leading-6 font-medium mt-4 text-white">
+                Since 2009, RH Construction USA Inc. has been dedicated to
+                delivering excellence in construction services, boasting over 15
+                years of expertise in home renovations.
+              </p>
+            </div>
           </div>
-          <div className="w-full md:w-5/12">
+          <img src="/assets/images/Group 73.png"
+            alt="" 
+            className="w-[224] h-[152px] absolute -bottom-[48px] left-0 rotate-0"/>
+          <div className="w-full md:w-5/12 relative">
             <img
-            src="/assets/images/person.png"
+            src="/assets/images/Group 21.png"
             alt="" 
             className="w-full h-full object-cover mb-0"
             />
-            <img src="/public/assets/images/Group"
+            <img src="/assets/images/Group 73.png"
             alt="" 
-            className="w-full h-full object-cover mb-0"/>
+            className="w-[224] h-[152px] absolute -bottom-[48px] right-0 rotate-180"/>
           </div>
         </div>
       </div>
