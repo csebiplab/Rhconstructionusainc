46b860e4665c5d0c552222ffdc204fce0c9c5455
Tawhidul3233 <mdtawhidulislam3233@gmail.comm>
2024-03-16 21:26:08 +0600

    contract and footer and services section pxl perfect

--- src/components/__home_components/ourServices/OurServices.jsx
+++ src/components/__home_components/ourServices/OurServices.jsx
@@ -79,7 +79,7 @@ export default function OurServices() {
 
   return (
     <div className=" bg-[#FFB7031A]">
-      <div className='custom-container py-14'>
+      <div className='custom-container lg:py-28 py-14'>
         <div className="grid grid-cols-1  lg:grid-cols-2 lg:gap-40 gap-4  p-5 items-end text-sm ">
 
           <div className="  ">
@@ -87,13 +87,13 @@ export default function OurServices() {
               <Image src={servicesIcon} className=' h-5 w-10 mr-2'></Image>
               <p>Services</p>
             </div>
-            <h2>Our Services</h2>
-            <p className=' lg:py-8 py-5'>
+            <h2 className=' lg:text-[36px] text-[20px]'>Our Services</h2>
+            <p className=' lg:py-8 py-6 lg:text-[20px] text-[14px] leading-10'>
               <b>Enjoy a stress-free construction service with our unique build process.RH Construction USA,</b> is your trusted partner for top-notch construction services, dedicated to bringing your dream project to life. From conceptualization to completion, we prioritize quality craftsmanship, timely delivery, and cost-effective solutions to meet and exceed your expectations.</p>
           </div>
 
           <div className=" ">
-            <p className=' lg:py-10 py-5 items-center'>
+            <p className=' lg:py-10 py-5 items-center lg:text-[20px] text-[14px] leading-10'>
               At our company, we understand that your dream project is a significant investment, and we take pride in delivering unparalleled construction service that reflects our unwavering commitment to customer satisfaction. Whether you're envisioning a residential masterpiece or a commercial endeavor, our experienced team is equipped to handle a diverse range of projects.
             </p>
           </div>
@@ -112,7 +112,7 @@ export default function OurServices() {
               onChange={handleCheckboxChange}
             />
             <span
-              className={`flex items-center space-x-[6px] lg:px-20 px-10 rounded-3xl py-2  text-sm font-medium ${!isChecked ? 'text-white bg-black ' : 'text-body-color'
+              className={`flex items-center space-x-[6px] lg:px-32 px-14 rounded-3xl lg:py-4  py-2  lg:text-[20px] text-sm font-medium ${!isChecked ? 'text-white bg-black ' : 'text-body-color'
                 }`}
             >
               <svg
@@ -133,7 +133,7 @@ export default function OurServices() {
               Interior
             </span>
             <span
-              className={`flex items-center space-x-[6px] lg:px-20 px-10 rounded-3xl py-2 text-sm font-medium ${isChecked ? ' text-white bg-black ' : 'text-body-color'
+              className={`flex items-center space-x-[6px] lg:px-32 px-14 rounded-3xl lg:py-4 py-2 lg:text-[20px] text-sm font-medium ${isChecked ? ' text-white bg-black ' : 'text-body-color'
                 }`}
             >
               <svg
@@ -149,17 +149,17 @@ export default function OurServices() {
           </label>
         </div>
 
-        <div>
+        <div className='lg:my-10 my-5'>
           {!isChecked &&
             <div className='grid lg:grid-cols-6 md:grid-cols-3 grid-cols-2 gap-5 gap-y-8'>
               {services.map((service, index) => (
                 <div key={index} className='col-span-1 border-2 rounded border-black'>
                   <div className="mx-auto rounded-lg shadow-lg bg-[#ffb80305] relative">
                     <div className=" py-7 ">
-                      <p className='mt-4 mx-auto text-center  text-xs'>{service.title} </p>
+                      <p className='mt-4 py-5  mx-auto text-center lg:text-[20px]  text-xs'>{service.title} </p>
                     </div>
                     <div className="absolute  top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                      <img className="w-14 h-14 bg-white rounded-full border-black" src={service.img.src} alt="Profile" />
+                      <img className=" lg:w-20 lg:h-20 w-14 h-14 bg-white rounded-full border-black" src={service.img.src} alt="Profile" />
                     </div>
                   </div>
                 </div>
@@ -172,10 +172,10 @@ export default function OurServices() {
                 <div key={index} className='col-span-1 border-2 rounded border-black'>
                   <div className="mx-auto rounded-lg shadow-lg bg-[#ffb80305] relative">
                     <div className=" py-7 ">
-                      <p className='mt-4 mx-auto text-center  text-xs'>{service.title} </p>
+                      <p className='mt-4 py-5 mx-auto text-center lg:text-[20px]  text-xs'>{service.title} </p>
                     </div>
                     <div className="absolute  top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                      <img className="w-14 h-14 bg-white rounded-full  border-black" src={service.img.src} alt="Profile" />
+                      <img className="lg:w-20 lg:h-20 w-14 h-14 bg-white rounded-full  border-black" src={service.img.src} alt="Profile" />
                     </div>
                   </div>
                 </div>
