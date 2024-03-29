70f73e8310b701fb296e92e2760b9d21c71d3a68
tonmoy <tonmoy3673@gmail.com>
2024-03-29 20:02:31 +0600

    resize achievement section

--- src/components/__home_components/Achievement.jsx
+++ src/components/__home_components/Achievement.jsx
@@ -5,8 +5,8 @@ const Achievement = () => {
   return (
     <>
       <div className="custom-container">
-      <div className="flex flex-wrap gap-2 xs:mt-5 md:mt-3 lg:mt-4">
-          <div className="md:w-[180px] md:h-[147.98px] md:flex-0 flex-1 my-class rounded-[8px] pt-[31.25px] text-center">
+      <div className="flex gap-2 xs:mt-5 md:mt-3 lg:mt-4">
+          <div className="md:w-[180px] md:h-[147.98px] md:flex-0 my-class rounded-[8px] pt-[31.25px] text-center">
             <h2 className="lg:text-[38px] text-lg font-bold lg:leading-[47.25px] md:mb-[5px] xs:mb-0 ">
               96%
             </h2>
@@ -14,7 +14,7 @@ const Achievement = () => {
               Recommended
             </p>
           </div>
-          <div className="md:w-[180px] md:h-[147.98px] md:flex-0 flex-1 my-class rounded-[8px] pt-[31.25px] text-center">
+          <div className="md:w-[180px] md:h-[147.98px] md:flex-0 my-class rounded-[8px] pt-[31.25px] text-center">
             <h2 className="lg:text-[38px] font-bold text-lg lg:leading-[47.25px] xs:mb-0 md:mb-[5px]">
               15
             </h2>
@@ -22,7 +22,7 @@ const Achievement = () => {
               Years In Business
             </p>
           </div>
-          <div className="md:w-[604px] md:h-[147.98px]  my-class rounded-[8px] px-[22.5px] md:pt-2 xs:pt-5">
+          <div className="md:w-[604px] md:h-[147.98px] my-class rounded-[8px] px-[22.5px] md:pt-2 xs:pt-5">
             <h3 className="text-base lg:text-lg font-bold lg:leading-[27px] mb-[5px]">
               Achievements
             </h3>
