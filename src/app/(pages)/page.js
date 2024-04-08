bcdaba2833d18f8cb833b9cc9fda8841179636d2
Oliulla <razasm29@gmail.com>
2024-04-09 01:37:23 +0600

    :bugs: Fixed robot.txt file

--- src/app/(pages)/page.js
+++ src/app/(pages)/page.js
@@ -1,42 +1,55 @@
-import { PageWrapper } from "@/components/Theme";
 import Home from "./Home";
 
-// import API from "@/config/API.config";
-// import { headers } from "next/headers";
+// Dynamic Metadata
+export async function generateMetadata() {
+  try {
+    const metaDataResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/home`);
+    const metaData = await metaDataResponse.json();
 
 
-// export async function generateMetadata() {
-//   const headersList = headers();
-//   const pathname = headersList.get("x-invoke-path") || "";
-//   try {
-//     const { data } = await API.get('/pages', {
-//       params: {
-//         route: pathname
-//       }
-//     });
-//     if (!data?.length) throw "no route found."
-//     return {
-//       title: data?.[0]?.title,
-//       keywords: data?.[0]?.keywords,
-//       description: data?.[0]?.description,
-//     }
-//   } catch (error) {
-//     console.log(error.message);
-//     return {
-//       title: "Construction Company in Brooklyn | RH Construction USA Inc.",
-//       keywords: "Home, RH Construction USA",
-//       description: "As one of the leading construction companies in Brooklyn, RH Construction USA Inc. offers top-notch general contractor services in Brooklyn."
-//     };
-//   }
-// }
+
+    // Fetch Google site verification URL
+    const googleVerificationResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/verificationUrl`);
+    const googleVerification = await googleVerificationResponse.json();
+
+    // Extract Google console key from the meta tag content
+    const googleConsoleKey = extractGoogleConsoleKey(googleVerification);
+
+    return {
+      title: metaData?.homeRouteAllMetaData[0]?.title,
+      description: metaData?.homeRouteAllMetaData[0]?.description,
+      keywords: metaData?.homeRouteAllMetaData[0]?.keywords,
+      verification: {
+        google: googleConsoleKey,
+      }
+    };
+  } catch (error) {
+    return {
+      title: "Construction Company in Brooklyn | RH Construction USA Inc.",
+      keywords: "Home, RH Construction USA",
+      description: "As one of the leading construction companies in Brooklyn, RH Construction USA Inc. offers top-notch general contractor services in Brooklyn."
+    };
+  }
+}
+
+function extractGoogleConsoleKey(googleVerification) {
+  try {
+    const { verificationUrl } = googleVerification ?? {};
+    const metaTagContent = verificationUrl[0]?.title;
+    const consoleKey = metaTagContent?.split(" ")[2]?.split("=")[1]?.slice(1, -1);
+    return consoleKey;
+  } catch (error) {
+    console.error('Error extracting Google console key:', error);
+  }
+}
+
+
+
 
 export default function Page() {
-  // return <PageBuilder/>
   return (
     <main className="min-h-screen">
-      <PageWrapper>
-        <Home />
-      </PageWrapper>
+      <Home />
     </main>
   )
 }
