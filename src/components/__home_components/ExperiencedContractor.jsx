777893e780a587dfc1dab18cca616aa4ea6951da
tonmoy <tonmoy3673@gmail.com>
2024-04-21 00:20:12 +0600

    added small images in experienced section

--- src/components/__home_components/ExperiencedContractor.jsx
+++ src/components/__home_components/ExperiencedContractor.jsx
@@ -1,5 +1,7 @@
 import cleaning from "@/components/__home_components/Images/cleaning.png";
 import cleaning1 from "@/components/__home_components/Images/cleaning1.png";
+import smallClean1 from "@/components/__home_components/Images/smallClean1.png";
+import smallClean2 from "@/components/__home_components/Images/smallClean2.png";
 import { smHeadingTexts } from "@/constants/smHeadingTexts";
 import Image from "next/image";
 import SmallHeadingWithIcon from "../ui/SmallHeadingWithIcon";
@@ -9,17 +11,19 @@ const ExperiencedContractor = () => {
     <div className="section-bg">
       <div className="py-9 md:py-10 lg:py-12 5xl:py-[120px] container">
         <div className="md:flex gap-4 lg:gap-10 xl:gap-12 3xl:gap-x-16 5xl:gap-x-20">
+
+          {/* =========== Desktop View ============ */}
           <div className="md:flex w-full md:w-1/2 gap-0 md:gap-[22px]">
-            <div>
+            <div  className="hidden md:block">
             <Image
               src={cleaning1}
               width={807}
               height={608}
-              className="w-[370px] h-[350px] md:w-full md:h-full lg:w-[399px] lg:h-[754px]"
+              className="md:w-full md:h-full lg:w-[399px] lg:h-[754px]"
               alt="cleaning1"
             />
             </div>
-            <div>
+            <div className="hidden md:block">
               <div className="text-center yellow-border py-3 md:py-8 my-2 md:my-0">
                 <h2 className="text-2xl lg:text-[80px] lg:leading-10">15+</h2>
                 <p className="text-lg lg:text-[30px] leading-normal mt-2 md:mt-4 px-0 md:px-1">Successful <br/> Years</p>
@@ -28,11 +32,40 @@ const ExperiencedContractor = () => {
               src={cleaning}
               width={807}
               height={608}
-              className="w-[370px] md:w-full md:h-3/4 lg:w-[386px] lg:h-[503px] mt-2 lg:mt-[24px]"
+              className="w-full md:w-full md:h-3/4 lg:w-[386px] lg:h-[503px] mt-2 lg:mt-[24px]"
               alt="cleaning"
             />
             </div>
           </div>
+
+          {/* ============= Mobile view ================ */}
+
+          <div className="block md:hidden">
+            <div>
+            <Image
+              src={smallClean1}
+              width={370}
+              height={280}
+              className="w-full"
+              alt="cleaning1"
+            />
+            </div>
+            
+              <div className="text-center yellow-border py-3 my-3">
+                <h2 className="text-2xl lg:text-[80px] lg:leading-10">15+</h2>
+                <p className="text-lg lg:text-[30px] leading-normal mt-2 md:mt-4 px-0 md:px-1">Successful <br/> Years</p>
+              </div>
+              <div>
+              <Image
+              src={smallClean2}
+              width={370}
+              height={230}
+              className="w-full"
+              alt="cleaning"
+            />
+            </div>
+          </div>
+
           <div className="mt-[26px] md:mt-0 w-full md:w-1/2">
             <div className="flex justify-center md:block">
               <SmallHeadingWithIcon
