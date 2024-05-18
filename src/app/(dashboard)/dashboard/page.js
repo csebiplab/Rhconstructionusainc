2576b4dfcf632e6237c209b14dfd5f5078cde1bf
Oliulla <razasm29@gmail.com>
2024-05-18 22:49:26 +0600

    change env

--- src/app/(dashboard)/dashboard/page.js
+++ src/app/(dashboard)/dashboard/page.js
@@ -1,9 +1,10 @@
+import ProjectDetailsForm from '@/components/__admin_dashboard_components/ProjectDetails/ProjectDetailsForm';
 import React from 'react';
 
 const DashboardMainPage = () => {
     return (
         <div>
-            dashboard main page
+            <ProjectDetailsForm />
         </div>
     );
 };
---
+++ src/components/__admin_dashboard_components/ProjectDetails/ProjectDetailsForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProjectDetailsForm = () => {
+  return <div>ProjectDetailsForm ProjectDetailsForm</div>;
+};
+
+export default ProjectDetailsForm;
