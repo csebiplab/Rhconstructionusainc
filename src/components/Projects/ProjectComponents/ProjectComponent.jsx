c11f43e4d3aabf5a9d12dfbdba9f426a05cbcc46
Oliulla <razasm29@gmail.com>
2024-05-23 00:16:16 +0600

    show no data found msg

--- src/components/Projects/ProjectComponents/ProjectComponent.jsx
+++ src/components/Projects/ProjectComponents/ProjectComponent.jsx
@@ -23,8 +23,6 @@ const ProjectsComponent = ({ projects }) => {
     return project?.projectType === projectType;
   });
 
-  //   console.log(filteredProjects, "filtered projects");
-
   return (
     <section className="container mb-48">
       <div className="flex justify-center mb-16">
@@ -42,64 +40,57 @@ const ProjectsComponent = ({ projects }) => {
           ))}
         </div>
       </div>
-      {/* <div>
-        {filteredProjects.map(
-          (
-            {
-              projectCat = "cat",
-              projectCatHeading = "head",
-              thumbPic = { name: "", url: "ji " },
-            },
-            index
-          ) => (
-            <div key={index}></div>
-          )
-        )}
-      </div> */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-9 project__parent p-7">
-        {filteredProjects.map(
-          (
-            {
-              projectCat = "cat",
-              projectCatHeading = "head",
-              thumbPic = { name: "", url: "" },
-            },
-            index
-          ) => (
-            <div key={index} className="relative w-full">
-              <img
-                src={thumbPic?.url}
-                alt={thumbPic?.name}
-                className="!h-72 rounded-md"
-              />
-              <div className="absolute bottom-3 px-3 w-full">
-                <h6 className="project__type project__typeTxt w-2/3 p-2">
-                  {projectCat}
-                </h6>
-                <div className="img__compnayHeading flex items-center justify-between py-4 px-2">
-                  <h5 className="project__typeAnCompnayTxt">
-                    {projectCatHeading}
-                  </h5>
-                  <span>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18"
-                      height="18"
-                      viewBox="0 0 18 18"
-                      fill="none"
-                    >
-                      <path
-                        d="M18 2L18 16C18 17.1 17.1 18 16 18L2 18C0.9 18 0 17.1 0 16L0 2C0 0.9 0.9 0 2 0L16 0C17.1 0 18 0.9 18 2ZM13 5.2H5.9L8 7.3L4.2 11.2L7 14L10.9 10.2L13 12.3L13 5.2Z"
-                        fill="black"
-                      />
-                    </svg>
-                  </span>
+
+      {filteredProjects?.length === 0 ? (
+        <p className="text-5xl text-center font-bold">No Data Found!!!</p>
+      ) : (
+        <>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-9 project__parent p-7">
+            {filteredProjects.map(
+              (
+                {
+                  projectCat = "cat",
+                  projectCatHeading = "head",
+                  thumbPic = { name: "", url: "" },
+                },
+                index
+              ) => (
+                <div key={index} className="relative w-full">
+                  <img
+                    src={thumbPic?.url}
+                    alt={thumbPic?.name}
+                    className="!h-72 rounded-md"
+                  />
+                  <div className="absolute bottom-3 px-3 w-full">
+                    <h6 className="project__type project__typeTxt w-2/3 p-2">
+                      {projectCat}
+                    </h6>
+                    <div className="img__compnayHeading flex items-center justify-between py-4 px-2">
+                      <h5 className="project__typeAnCompnayTxt">
+                        {projectCatHeading}
+                      </h5>
+                      <span>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="18"
+                          height="18"
+                          viewBox="0 0 18 18"
+                          fill="none"
+                        >
+                          <path
+                            d="M18 2L18 16C18 17.1 17.1 18 16 18L2 18C0.9 18 0 17.1 0 16L0 2C0 0.9 0.9 0 2 0L16 0C17.1 0 18 0.9 18 2ZM13 5.2H5.9L8 7.3L4.2 11.2L7 14L10.9 10.2L13 12.3L13 5.2Z"
+                            fill="black"
+                          />
+                        </svg>
+                      </span>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          )
-        )}
-      </div>
+              )
+            )}
+          </div>
+        </>
+      )}
     </section>
   );
 };
