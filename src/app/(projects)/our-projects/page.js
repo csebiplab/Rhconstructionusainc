a7a9fdbf9ce5cb71c1e78e50b1d1b7e5e90c6938
Oliulla <razasm29@gmail.com>
2024-05-24 02:09:40 +0600

    comment for deploy api

--- src/app/(projects)/our-projects/page.js
+++ src/app/(projects)/our-projects/page.js
@@ -1,46 +1,45 @@
 import ProjectsComponent from "@/components/Projects/ProjectComponents/ProjectComponent";
 
-// export async function generateMetadata() {
+export async function generateMetadata() {
 
-//   try {
-//     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/projects`);
-//     if (!response.ok) {
-//       console.log('Network response was not ok.');
-//     }
-//     const data = await response.json();
-//     const { title, description, keywords } = data?.metaData[0] ?? {}
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/projects`);
+    if (!response.ok) {
+      console.log('Network response was not ok.');
+    }
+    const data = await response.json();
+    const { title, description, keywords } = data?.metaData[0] ?? {}
 
-//     return {
-//       title: title,
-//       keywords: keywords,
-//       description: description,
-//     };
-//   } catch (error) {
-//     // console.log(error.message);
-//     return {
-//       title: 'Best General Contractor in Brooklyn |  RH Construction USA Inc.',
-//       description: 'Best general contractor in Brooklyn, specializing in home renovations, bathroom renovations & kitchen remodeling, roofing, and construction services.',
-//       keywords: 'General Contractor, General Contractor in Brooklyn, RH Construction USA Inc, home renovations, bathroom & kitchen remodeling, roofing',
-//     }
-//   }
-// }
+    return {
+      title: title,
+      keywords: keywords,
+      description: description,
+    };
+  } catch (error) {
+    // console.log(error.message);
+    return {
+      title: 'Best General Contractor in Brooklyn |  RH Construction USA Inc.',
+      description: 'Best general contractor in Brooklyn, specializing in home renovations, bathroom renovations & kitchen remodeling, roofing, and construction services.',
+      keywords: 'General Contractor, General Contractor in Brooklyn, RH Construction USA Inc, home renovations, bathroom & kitchen remodeling, roofing',
+    }
+  }
+}
 
 export default async function Page() {
-  // const res = await getData()
+  const res = await getData()
 
-  // if (res?.status !== 200) {
-  //   <p>Failed To Get Data!!!</p>
-  // }
+  if (res?.status !== 200) {
+    <p>Failed To Get Data!!!</p>
+  }
 
-  // const projects = res?.data;
+  const projects = res?.data;
 
   // console.log(projects)
 
   return (
     <main>
       <>
-        {/* <ProjectsComponent projects={projects} /> */}
-        deploy api
+        <ProjectsComponent projects={projects} />
       </>
     </main>
   );
