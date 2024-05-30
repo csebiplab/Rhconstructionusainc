5761ab19a86071e3ea5e68b472b89864d73e846e
Oliulla <razasm29@gmail.com>
2024-05-31 02:07:43 +0600

    refactor

--- src/app/(pages)/contact/page.js
+++ src/app/(pages)/contact/page.js
@@ -2,58 +2,34 @@ import ContactUs from "./ContactUs";
 
 export async function generateMetadata() {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/contactUs`);
-    const metaData = await res.json();
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/contactUs`, { cache: 'no-store' });
 
+    if (!response.ok) {
+      console.error('Failed to fetch metadata');
+    }
+
+    const metaData = await response.json();
+    const { title = "Contact Us | RH Construction USA", description = "Get in touch with RH Construction USA for all your construction needs.", keywords = "Contact Us, RH Construction USA" } = metaData?.contactRouteAllMetaData?.[0] || {};
 
     return {
-      title: metaData?.contactRouteAllMetaData[0]?.title,
-      description: metaData?.contactRouteAllMetaData[0]?.description,
-      keywords: metaData?.contactRouteAllMetaData[0]?.keywords
-
-    }
+      title,
+      description,
+      keywords,
+    };
   } catch (error) {
-    console.error(error)
+    console.error('Error fetching metadata:', error);
+    return {
+      title: "Contact Us | RH Construction USA",
+      description: "Get in touch with RH Construction USA for all your construction needs.",
+      keywords: "Contact Us, RH Construction USA",
+    };
   }
-
-
 }
 
-
-
-// import API from "@/config/API.config";
-// import { headers } from "next/headers";
-// export async function generateMetadata() {
-//   const headersList = headers();
-//   const pathname = headersList.get("x-invoke-path") || "";
-//   try {
-//     const {data}= await API.get('/pages',{
-//       params:{
-//         route:pathname
-//       }
-//     });
-//     if(!data?.length) throw "no route found."
-//     return {
-//       title: data?.[0]?.title,
-//       keywords: data?.[0]?.keywords,
-//       description: data?.[0]?.description,
-//     }
-//   } catch (error) {
-//    console.log(error.message);
-//    return {
-//       title: "Contact Us | RH Construction USA",
-//       keywords: "Contact Us, RH Construction USA",
-//     };
-//   }
-// }
-
 export default function Page() {
-
   return (
     <main>
-      {/* <PageWrapper> */}
       <ContactUs />
-      {/* </PageWrapper> */}
-    </main >
+    </main>
   );
 }
