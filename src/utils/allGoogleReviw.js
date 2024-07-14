e8e1a9f6ada5ad5ba47e48c10235c4a22e05e63d
Oliulla <razasm29@gmail.com>
2024-07-14 23:26:23 +0600

    all google reviews

---
+++ src/utils/allGoogleReviw.js
@@ -0,0 +1,331 @@
+export const allGoogleReviws = [
+    {
+        "text": "Abdul installed rubber roofing, waterproofed my chimney, isolated a major leak, and did clean interior drywall. All of it was done very well, and he and his crew are professional, polite and honest. Highly recommended.",
+        "name": "Jeff McLeod",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 1
+    },
+    {
+        "text": "My ceiling was on the brink of collapse and these guys came in quickly and did a great job removing the old ceiling and installing a new one. They worked hard and finished on time despite some structural setbacks which required some additional work. Would highly recommend.",
+        "name": "Pete Connolly",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 2
+    },
+    {
+        "text": "Absolutely transformed our bathrooms!! I love the end result, their team worked in a timely and professional manner, and worked to offer some of the best prices I’ve ever seen! I will definitely be bring them for all our future projects.",
+        "name": "Anastasia Brown",
+        "positives": "Responsiveness, Quality, Professionalism, Value",
+        "ratings": 5,
+        "services": "",
+        "id": 3
+    },
+    {
+        "text": "RH Construction remodeled our kitchen, including installing new cabinets, backsplash and appliances, and they were amazing. From the initial consultation to the design and selection of cabinets and tile to the installation, they were thorough, professional and efficient. Abdul had great suggestions for colors and materials and the end result is beautiful (see below for before and after). The project ran exactly on time and Abdul was very communicative throughout. His crew worked cleanly and on schedule. Overall a great experience—I highly recommend RH Construction.",
+        "name": "Bailey Somers",
+        "positives": "Responsiveness, Punctuality, Quality, Professionalism, Value",
+        "ratings": 5,
+        "services": "Tile work installation, Tile work replacement, Paint indoors, Remodeling",
+        "id": 4
+    },
+    {
+        "text": "R H construction USA is very professional job did for my commercial building and remove the violation thank you again. I will hire and recommend the company again and again.",
+        "name": "Adolphus Anosike",
+        "positives": "Responsiveness, Professionalism, Value",
+        "ratings": 5,
+        "services": "",
+        "id": 5
+    },
+    {
+        "text": "I had an excellent experience with RH construction. They replaced my roof, back wall and built a private roof-top deck as shown in the picture. They are professional, reliable, efficient, communicative and gave a fair price. I would highly recommend RH Construction and look forward to future projects with them.",
+        "name": "Will Geary",
+        "positives": "Responsiveness, Quality, Professionalism, Value",
+        "ratings": 5,
+        "services": "",
+        "id": 6
+    },
+    {
+        "text": "We hired RH Construction to work on the windows of the church and repair the roof.  The team was professional and did a great job.  They gave us a good and fair price for the work.  I would highly recommend them.",
+        "name": "The King's Chapel",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 7
+    },
+    {
+        "text": "Tipu Petroleum is organizing Lube gala, In which you can get Bikes, Microwave oven, mobile phones, sandwich maker and many more, just buy lube oil from Tipu Petroleum and Enter in Lucky draw.",
+        "name": "Waseem Usman",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 8
+    },
+    {
+        "text": "Minal and RH Construction was very professional and did great work on my home. He offered advise and did the little extras to finish the job. I highly recommend him!",
+        "name": "Vince Alban",
+        "positives": "Responsiveness, Punctuality, Quality, Professionalism, Value",
+        "ratings": 5,
+        "services": "General construction",
+        "id": 9
+    },
+    {
+        "text": "Really RH construction does a very good job with their honesty, He and his team are very smart and intelligent, they completed the job on time. But I was surprised to see a review! A lady left a 1 star review! A few days ago the Miner did the work of repairing the balcony of our house, I know this lady, she is my neighbor, mentally sick and bad tempered! Minar didn't finish the job it's not Minar's problem but that's the problem of that bad tempered lady, Minar refunded the lady. I know Minar, Very good man and works on time! I recommend rh construction USA inc to everyone!",
+        "name": "Mario Lindsey",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 10
+    },
+    {
+        "text": "Remodeling my resume and it brought me back a couple years, thank you RH CONSTRUCTION USA INC for everything awesome to work with and pure team to be around 😁. …",
+        "name": "Danny Perugini (DooDat)",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 11
+    },
+    {
+        "text": "Great contractor! Begins on time and ends on time, personable, meticulous and professional.",
+        "name": "Nichole L Hunter",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 12
+    },
+    {
+        "text": "We hired the RH construction to work on the church. They first fixed the ceiling in the bathroom. Then, we hired them to paint the outside windows. They scraped and painted all the windows including the main entrance window. We then hired them to fix the two back flat roofs on the building. Excellent job! They gave us a good price on all the jobs. The men were honest, kind and polite. I would highly recommend them.",
+        "name": "T. G.",
+        "positives": "Responsiveness, Punctuality, Quality, Professionalism, Value",
+        "ratings": 5,
+        "services": "Roof repair, Roof installation, Exterior painting",
+        "id": 13
+    },
+    {
+        "text": "This is the first time we search for a company to help us repair and paint our moms entire 1 bedroom apartment. We needed major work including plastering and painting in every room. We were a bit nervous because we didnt know what company to select. I thank God we found RH Construction the workmanship they did was supentious, all the workers were dedicated, respectful and hardworking. The end result after they painted was absolutely seamless. If this is your first time searching for a company please give them a call they really gave us such peace of mind and best of all they prove to be true experts. Thank you RH Construction we are so grateful.",
+        "name": "Elsie Martinez",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 14
+    },
+    {
+        "text": "For my basement remodeling project, I selected RH Construction usa inc , and they did an excellent job! They were competent and professional, and the outcome exceeded my expectations.",
+        "name": "Gretchen Jenkins",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 15
+    },
+    {
+        "text": "I am extremely pleased with the electrical work that was done in my home. Minar and his team were able to adapt to and overcome the variety of obstacles that they encountered while repairing and streamlining my circuitry. His staff is well trained, professional, friendly, hardworking, and efficient. Minar offers thorough explanations of the work that is required before starting the job, and also outlines any potential problems (before they become problems). I believe that he is fair and honest, and will not hesitate to hire him again for the variety of work that my home requires...",
+        "name": "randy jones",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 16
+    },
+    {
+        "text": "Made our old outdated apartment come alive again.  Fresh paint, fixed any bumps or cracks in walls and ceilings, worked fast and did excellent work!  Would definitely hire again.  Easy to contact and did what I asked- happy customer Here!",
+        "name": "Julie Collins",
+        "positives": "Responsiveness, Punctuality, Quality, Professionalism, Value",
+        "ratings": 5,
+        "services": "Paint indoors, Interior painting",
+        "id": 17
+    },
+    {
+        "text": "Minar built us a half bathroom and a number of side projects including closet installation, replacing a rotted exterior doorway, as well as multiple cement work jobs to name a few. I have not worked with too many contractors but it seems that many of them neglect to communicate details that I would like to know. Minar, on the other hand, despite the fact that English is not his first language, worked hard to keep communication open during the life of the project. Repeatedly, he checked in to make sure I was happy with not only the work but the way in which it was being done. He and his crew worked quickly and with great care for my pregnant wife, our neighbors, and everyone’s schedules. Be careful though, make sure to go over details ahead of time because they work so fast that sometimes they will complete part of the project before you expect them to. Almost always a pleasant surprise. As our main project progressed numerous other little jobs came up. Minar was not only capable of handling a variety of different types of work, but he worked with us in an open manner to come to a fair and agreeable price. I was so pleased working with him that I recommended him to our condo board where he has continue to deliver very satisfactory work for our building. And now I recommend him to you.",
+        "name": "Matt McCroskey",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 18
+    },
+    {
+        "text": "Minar is very professional and takes pride in his work. He is always on time, and alerts me when he's coming and begins work exactly when he says he will which is very important to me when looking for a professional contractor. I am having my exterior Landmark townhouse painted and so far am very happy with Minar, his staff and the results.",
+        "name": "Afua M. Preston",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 19
+    },
+    {
+        "text": "Hello, We used RH Construction Company for our sidewalk repair job. They did an excellent job by showing up on time and securing the area of the work. They completed the job in a timely manner and Management and Resident really were satisfied with the perfect job that RH did. They are very reliant and we will use this company in the future for other jobs.",
+        "name": "Kestrel Concierge",
+        "positives": "",
+        "ratings": 5,
+        "services": "Service not listed",
+        "id": 20
+    },
+    {
+        "text": "Fantastic experience with Minar. They are professional and responsive. They have a lot of stoop, facade, and siding experience. They completely transformed the front of our house. Minar is also very kind which makes it easy to work with him. We went through a few quotes and Minar's proved to be the most honest!",
+        "name": "Zak Cherrabi",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 21
+    },
+    {
+        "text": "We live in a house that’s 35 years old. Lately, we noticed, our house started to give out foul smell in drains and walls used to remain wet. This is when I called up RH Construction Usa, and their ethical & highly intelligent workers came over to fix my house. At first sight they said, problem is with pipes. Upon careful evaluation, they changed the drain pipes, cleaning away all stuck material from the area and wrapped up the work in not more than 2 days. I would highly recommend to consider them as your all time construction companies.",
+        "name": "Evelyn Thomas",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 22
+    },
+    {
+        "text": "Hired Minar because he had just done work at the neighbor's house. The job was replacing about a quarter of a very old brick wall which was crumbling. He did a beautiful job, communicated well through the entire phase and they cleaned up nicely afterwards. I had given them the key to the house since I had been traveling. When I left the wall was crumbling, when I returned it was all done as promised.",
+        "name": "Alexander",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 23
+    },
+    {
+        "text": "Minar is fair and professional. He and his team are punctual, thorough and knowledgeable. I have an old turn of the century home with lots of challenges. Minar was able to walk me through everything and figure out ways around obstacles. I would definitely recommend him.",
+        "name": "Angela Harding",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 24
+    },
+    {
+        "text": "Minar and his team did an excellent job repairing our ceiling after a very bad leak. They also did a great job replacing broken pipes.",
+        "name": "jonathan sheehan",
+        "positives": "Professionalism",
+        "ratings": 5,
+        "services": "",
+        "id": 25
+    },
+    {
+        "text": "Minar and his team did great work insulating soundproofing and doing a drop ceiling for me!",
+        "name": "Carson McCalley",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 26
+    },
+    {
+        "text": "From the time we meet the boss was very professional and explained into detail and made me aware before he started projects. Cery kind and respectful. Great work Thank RH construction usa",
+        "name": "Loretta Dunn",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 27
+    },
+    {
+        "text": "We hired the company for painting and installing a toilet. They were fast and efficient. We had some issues with paint edges, but overall the work was great. Thanks!",
+        "name": "N B",
+        "positives": "",
+        "ratings": 4,
+        "services": "",
+        "id": 28
+    },
+    {
+        "text": "Minar and his team did a bunch of foundational work on our Brooklyn brownstone, which included new outside stucco back wall, roof, gutter and flashing installation, basement wall filling and sealing, new basement sewage/gas/water pipes, painting and patching of two floors (including around intricate original plaster work, minor electrical installation, front gas light installation and front door stripping/sanding/refinishing. Throughout the entire process, Minar was extremely responsive by call/text and available until late at night if needed. His team was timely, responsive, and efficient -- arriving when they said they would, taking good care of their work, and finishing on schedule. They did a great job with everything they did, and we would recommend them to others.",
+        "name": "Martin Wexler",
+        "positives": "Responsiveness, Punctuality, Quality",
+        "ratings": 5,
+        "services": "",
+        "id": 29
+    },
+    {
+        "text": "They fixed a patch in my bathroom ceiling and made the entire ceiling like brand new! Very friendly, fast and professional! I will use them in the future if I ever need it!",
+        "name": "genevieve platt",
+        "positives": "Responsiveness, Quality, Professionalism, Value",
+        "ratings": 5,
+        "services": "",
+        "id": 30
+    },
+    {
+        "text": "The workers did a great job. They worked very industriously and the room which was being updated looks great. Every part of the job was very carefully done. I highly recommend this company.",
+        "name": "Jane Birzin",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 31
+    },
+    {
+        "text": "Outstanding renovations on my church. Very professional concise and cordial. Abdul very concerned and pays attention to detail. Highly recommended to get the job done.",
+        "name": "Winston Howerton",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 32
+    },
+    {
+        "text": "I hired them to regrade and waterproof an elevated deck. They did a great job and fixed the problem quickly. I would definitely hire them again.",
+        "name": "Camden Place",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 33
+    },
+    {
+        "text": "Excellent work performance in our buildings balcony tile work by RH construction USA we all use the company again",
+        "name": "Mei Yee Chan",
+        "positives": "Responsiveness, Quality, Professionalism, Value",
+        "ratings": 5,
+        "services": "",
+        "id": 34
+    },
+    {
+        "text": "Good work fast and professional",
+        "name": "Christian Bellucci",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 35
+    },
+    {
+        "text": "The job we had done was a wall that divided a big area. I was totally satisfied with the job. The job was completed exactly as I was told it would be. Punctually on time.",
+        "name": "Miriam Jerusalem",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 36
+    },
+    {
+        "text": "The job was done very quickly, the workers were very efficient, arrived on time, and were very respectful. My parents are very happy.",
+        "name": "florence henry",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 37
+    },
+    {
+        "text": "Got my apartment renovated by RH Construction beautiful and amazing experience Love my new kitchen and bathroom highly recommend then to everybody!!!!",
+        "name": "Samantha Hoole",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 38
+    },
+    {
+        "text": "Very Professional, Amazing work. I would highly recommend!",
+        "name": "Jackson Huang",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 39
+    },
+    {
+        "text": "Fast and beautiful job. A+++++",
+        "name": "nelson joa",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 40
+    },
+    {
+        "text": "Professional, thorough, timely response. Highly recommend",
+        "name": "Pierre Carras",
+        "positives": "",
+        "ratings": 5,
+        "services": "",
+        "id": 41
+    }
+
+]
