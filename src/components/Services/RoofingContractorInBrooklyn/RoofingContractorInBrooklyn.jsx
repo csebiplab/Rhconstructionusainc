c258293f8c644d013b37dd759b54c07730bfb19b
Kongkon Jowarder <kongkon4545@gmail.com>
2024-08-20 23:45:42 +0600

    roofingContractorInBrooklyn, CommercialRoofing, MetalRoofing, RoofingContractors, SteelRooof, TileRoof, Contact us and areas section add

--- src/components/Services/RoofingContractorInBrooklyn/RoofingContractorInBrooklyn.jsx
+++ src/components/Services/RoofingContractorInBrooklyn/RoofingContractorInBrooklyn.jsx
@@ -6,6 +6,13 @@ import RatingComponent from "@/components/__home_components/RatingComponent";
 import AboutOur from "./AboutOur/AboutOur";
 import Expert from "./Expert/Expert";
 import ResidentialRoofing from "./ResidentialRoofing/ResidentialRoofing";
+import MetalRoofing from "./MetalRoofing/MetalRoofing";
+import SteelRoof from "./SteelRoof/SteelRoof";
+import TileRoof from "./TileRoof/TileRoof";
+import ContactUs from "@/components/bathroom-reno-components/contactUs/ContactUs";
+import LocationMap from "@/components/bathroom-reno-components/LocationMap/LocationMap";
+import RoofingContractors from "./RoofingContractors/RoofingContractors";
+import CommercialRoofing from "./CommercialRoofing/CommercialRoofing";
 
 const RoofingContractorInBrooklyn = () => {
   return (
@@ -28,6 +35,37 @@ const RoofingContractorInBrooklyn = () => {
 
       <section>
         <ResidentialRoofing/>
+      </section>
+
+      <section>
+        <CommercialRoofing/>
+      </section>
+      
+      <section>
+        <MetalRoofing/>
+      </section>
+
+      <section>
+        <SteelRoof/>
+      </section>
+
+      <section>
+        <TileRoof/>
+      </section>
+
+      <section>
+        <RoofingContractors/>
+      </section>
+
+
+
+
+      <section>
+      <LocationMap />
+      </section>
+
+      <section>
+        <ContactUs />
       </section>
 
     </div>
