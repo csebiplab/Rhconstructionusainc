b008e158a6db0eac859cb5d088e889b19738e2ee
Kongkon Jowarder <kongkon4545@gmail.com>
2024-08-23 22:31:36 +0600

    costIdeas, projectGallery, readyToWorkWithUs, RoofingRepairCompany, RoofRepairServices, whyHireUs and responsive issue solve

--- src/components/Services/RoofRepairInBrooklyn/Dependable/Dependable.jsx
+++ src/components/Services/RoofRepairInBrooklyn/Dependable/Dependable.jsx
@@ -7,8 +7,8 @@ const Dependable = () => {
   return (
     <div className="bg___color">
       <div className="container common__padding__top">
-        <div className=" grid md:grid-cols-6 grid-cols-1 gap-10 md:mx-0 mx-[35px]">
-          <div className="md:col-span-3">
+        <div className=" grid md:grid-cols-6 grid-cols-1 gap-10">
+          <div className="order-2 md:order-1 md:col-span-3">
             <div className="flex md:justify-normal justify-center md:items-start items-center">
             <HeadingIcon text={headingIconText.dependable__headingIconText}/>
             </div>
@@ -22,14 +22,14 @@ const Dependable = () => {
             RH Construction USA Inc. provides dependable roofing repair services in Brooklyn, NY, ensuring your roof is in excellent condition with reliable, high-quality repairs. Our experienced team addresses all types of issues, from leaks and damaged shingles to flat and metal roof problems, using top-grade materials and advanced techniques. Committed to customer satisfaction, we offer prompt, professional service to restore your roof's integrity and protect your property. Our dependable roofing repair services are designed to address both minor and major roofing issues with precision and care. We start with a thorough inspection to accurately diagnose problems and provide tailored solutions that meet your specific needs. Whether you need emergency repairs or routine maintenance, our skilled team ensures that every repair is completed efficiently and effectively. We pride ourselves on transparent communication, keeping you informed throughout the repair process, and delivering results that enhance the durability and appearance of your roof.
             </p>
           </div>
-          <div className="md:col-span-3 flex justify-center items-center ">
+          <div className="order-1 md:order-2 md:col-span-3 flex justify-center items-center ">
             <div className="">
               <Image
                 src="/assets/roof_repair/Dependable.png"
                 width={810}
                 height={600}
                 alt="Dependable image"
-                className="w-[810px] h-[600px]"
+                className="w-[810px] h-[350px] md:h-[600px]"
               />
             </div>
           </div>
